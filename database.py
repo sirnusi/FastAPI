6339c4575019ebc18141d52d0449d12f2d5cb53c
sirnusi <davidsanusi360@gmail.com>
2026-07-06 14:56:01 +0200

    Updated database

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:olajide@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
